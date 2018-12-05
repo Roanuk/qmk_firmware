3b79616a94ea806dc321d744285ad674daadadb1
Cole Harrison <Douglas.Harrison@microsoft.com>
2018-12-05 12:51:00 -0800

    Added Backlit support + keys

--- keyboards/ergodox_ez/keymaps/roanuk/keymap.c
+++ keyboards/ergodox_ez/keymaps/roanuk/keymap.c
@@ -53,16 +53,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 1: Symbol Layer
 *
 * ,--------------------------------------------------.      ,--------------------------------------------------.
-* |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |      |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
+* |RGB_HUI |  F1  |  F2  |  F3  |  F4  |  F5  |      |      |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
 * |--------+------+------+------+------+-------------|      |------+------+------+------+------+------+--------|
-* |        |   !  |   @  |   {  |   }  |   |  |      |      |      |   Up |   7  |   8  |   9  |   *  |   F12  |
+* |RGB_HUD |   !  |   @  |   {  |   }  |   |  |      |      |      |   Up |   7  |   8  |   9  |   *  |   F12  |
 * |--------+------+------+------+------+------|      |      |      |------+------+------+------+------+--------|
-* |        |   #  |   $  |   (  |   )  |   `  |------|      |------| Down |   4  |   5  |   6  |   +  |        |
+* |RGB_SAI |   #  |   $  |   (  |   )  |   `  |------|      |------| Down |   4  |   5  |   6  |   +  | RGB_VAI|
 * |--------+------+------+------+------+------|      |      |      |------+------+------+------+------+--------|
-* |        |   %  |   ^  |   [  |   ]  |   ~  |      |      |      |   &  |   1  |   2  |   3  |   \  |        |
+* |RGB_SAD |   %  |   ^  |   [  |   ]  |   ~  |      |      |      |   &  |   1  |   2  |   3  |   \  | RGB_VAD|
 * `--------+------+------+------+------+-------------'      `-------------+------+------+------+------+--------'
-*   |      |      |      |      |      |                                  |      |    . |   0  |   =  |        |
-*   `----------------------------------'                                  `------------------------------------'
+*  |RGB_TOG|      |      |      |      |                                  |      |    . |   0  |   =  |RGB_MOD|
+*  `-----------------------------------'                                  `-----------------------------------'
 *                                        ,-------------.  ,-------------.
 *                                        |Ctr+X |      |  |      | Ctr+S|
 *                                 ,------|------|------|  |------+------+------.
@@ -74,20 +74,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // SYMBOLS
 [SYMB] = LAYOUT_ergodox(
        // left hand
-       KC_TRNS,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_TRNS,
-       KC_TRNS,KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,KC_TRNS,
-       KC_TRNS,KC_HASH,KC_DLR, KC_LPRN,KC_RPRN,KC_GRV,
-       KC_TRNS,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,KC_TRNS,
-       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
+       RGB_HUI,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_TRNS,
+       RGB_HUD,KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,KC_TRNS,
+       RGB_SAI,KC_HASH,KC_DLR, KC_LPRN,KC_RPRN,KC_GRV,
+       RGB_SAD,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,KC_TRNS,
+       RGB_TOG,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
                                        LCTL(KC_X),KC_TRNS,
                                                KC_TRNS,
                                LCTL(KC_V),LCTL(KC_C),KC_TRNS,
        // right hand
        KC_TRNS, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
        KC_TRNS, KC_UP,   KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
-                KC_DOWN, KC_4,   KC_5,    KC_6,    KC_PLUS, KC_TRNS,
-       KC_TRNS, KC_AMPR, KC_1,   KC_2,    KC_3,    KC_BSLS, KC_TRNS,
-                         KC_TRNS,KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
+                KC_DOWN, KC_4,   KC_5,    KC_6,    KC_PLUS, RGB_VAI,
+       KC_TRNS, KC_AMPR, KC_1,   KC_2,    KC_3,    KC_BSLS, RGB_VAD,
+                         KC_TRNS,KC_DOT,  KC_0,    KC_EQL,  RGB_MOD,
        KC_TRNS, LCTL(KC_S),
        KC_TRNS,
        KC_TRNS, LCTL(KC_Z), LCTL(KC_Y)
