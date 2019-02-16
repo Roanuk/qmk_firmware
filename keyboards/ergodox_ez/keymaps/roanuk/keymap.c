8c5fea35d5dff539f67898febc7f1694d7a4d411
Cole Harrison <douglas.cole.harrison@gmail.com>
2019-02-15 17:15:43 -0800

    Moved keys to leave primary thumb keys open. Also moved backlight colors to 3rd layer

--- keyboards/ergodox_ez/keymaps/roanuk/keymap.c
+++ keyboards/ergodox_ez/keymaps/roanuk/keymap.c
@@ -53,58 +53,58 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 1: Symbol Layer
 *
 * ,--------------------------------------------------.      ,--------------------------------------------------.
-* |RGB_HUI |  F1  |  F2  |  F3  |  F4  |  F5  |      |      |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
+* |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |      |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
 * |--------+------+------+------+------+-------------|      |------+------+------+------+------+------+--------|
-* |RGB_HUD |   !  |   @  |   {  |   }  |   |  |      |      |      |   Up |   7  |   8  |   9  |   *  |   F12  |
+* |        |   !  |   @  |   {  |   }  |   |  |      |      |      |   Up |   7  |   8  |   9  |   *  |   F12  |
 * |--------+------+------+------+------+------|      |      |      |------+------+------+------+------+--------|
-* |RGB_SAI |   #  |   $  |   (  |   )  |   `  |------|      |------| Down |   4  |   5  |   6  |   +  | RGB_VAI|
+* |        |   #  |   $  |   (  |   )  |   `  |------|      |------| Down |   4  |   5  |   6  |   +  |        |
 * |--------+------+------+------+------+------|      |      |      |------+------+------+------+------+--------|
-* |RGB_SAD |   %  |   ^  |   [  |   ]  |   ~  |      |      |      |   &  |   1  |   2  |   3  |   \  | RGB_VAD|
+* |        |   %  |   ^  |   [  |   ]  |   ~  |      |      |      |   .  |   1  |   2  |   3  |   \  |        |
 * `--------+------+------+------+------+-------------'      `-------------+------+------+------+------+--------'
-*  |RGB_TOG|      |      |      |      |                                  |      |    . |   0  |   =  |RGB_MOD|
+*  |       |      |      |      |      |                                  |      |      |   0  |   =  |       |
 *  `-----------------------------------'                                  `-----------------------------------'
 *                                        ,-------------.  ,-------------.
 *                                        |Ctr+X |      |  |      | Ctr+S|
 *                                 ,------|------|------|  |------+------+------.
-*                                 |      |      |      |  |      |      |      |
-*                                 |Ctr+V |Ctr+C |------|  |------| Ctr+Z| Ctr+Y|
-*                                 |      |      |      |  |      |      |      |
+*                                 |      |      |Ctr+C |  |Ctr+Y |      |      |
+*                                 |      |      |------|  |------|      |      |
+*                                 |      |      |Ctr+V |  |Ctr+Z |      |      |
 *                                 `--------------------'  `--------------------'
 */
 // SYMBOLS
 [SYMB] = LAYOUT_ergodox(
        // left hand
-       RGB_HUI,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_TRNS,
-       RGB_HUD,KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,KC_TRNS,
-       RGB_SAI,KC_HASH,KC_DLR, KC_LPRN,KC_RPRN,KC_GRV,
-       RGB_SAD,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,KC_TRNS,
-       RGB_TOG,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
+       KC_TRNS,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_TRNS,
+       KC_TRNS,KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,KC_TRNS,
+       KC_TRNS,KC_HASH,KC_DLR, KC_LPRN,KC_RPRN,KC_GRV,
+       KC_TRNS,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,KC_TRNS,
+       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
                                        LCTL(KC_X),KC_TRNS,
-                                               KC_TRNS,
-                               LCTL(KC_V),LCTL(KC_C),KC_TRNS,
+                                               LCTL(KC_V),
+                               KC_TRNS,KC_TRNS,LCTL(KC_C),
        // right hand
        KC_TRNS, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
        KC_TRNS, KC_UP,   KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
-                KC_DOWN, KC_4,   KC_5,    KC_6,    KC_PLUS, RGB_VAI,
-       KC_TRNS, KC_AMPR, KC_1,   KC_2,    KC_3,    KC_BSLS, RGB_VAD,
-                         KC_TRNS,KC_DOT,  KC_0,    KC_EQL,  RGB_MOD,
+                KC_DOWN, KC_4,   KC_5,    KC_6,    KC_PLUS, KC_TRNS,
+       KC_TRNS, KC_DOT,  KC_1,   KC_2,    KC_3,    KC_BSLS, KC_TRNS,
+                         KC_TRNS,KC_TRNS, KC_0,    KC_EQL,  KC_TRNS,
        KC_TRNS, LCTL(KC_S),
-       KC_TRNS,
-       KC_TRNS, LCTL(KC_Z), LCTL(KC_Y)
+       LCTL(KC_Y),
+       LCTL(KC_Z), KC_TRNS, KC_TRNS
 ),
 /*  Keymap 2: Media and mouse keys
  *
  * ,--------------------------------------------------.      ,--------------------------------------------------.
- * |        |      |      |      |      |      |      |      |      |      |      |      |      |      |        |
+ * | RGB_HUI|      |      |      |      |      |      |      |      |      |      |      |      |      |        |
  * |--------+------+------+------+------+-------------|      |------+------+------+------+------+------+--------|
- * |        |      |      | MsUp |      |      |      |      |      |      |      |      |      |      |        |
+ * | RGB_HUD|      |      | MsUp |      |      |      |      |      |      |      |      |      |      |        |
  * |--------+------+------+------+------+------|      |      |      |------+------+------+------+------+--------|
- * |        |      |MsLeft|MsDown|MsRght|      |------|      |------|      |      |      |      |      |  Play  |
+ * | RGB_SAI|      |MsLeft|MsDown|MsRght|      |------|      |------|      |      |      |      |      | RGB_VAI|
  * |--------+------+------+------+------+------|      |      |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |      |      |      |      | Prev | Next |      |        |
+ * | RGB_SAD|      |      |      |      |      |      |      |      |      |      | Prev | Next |      | RGB_VAD|
  * `--------+------+------+------+------+-------------'      `-------------+------+------+------+------+--------'
- *   |      |      |      | Lclk | Rclk |                                       |VolUp |VolDn | Mute |      |      |
- *   `----------------------------------'                                       `----------------------------------'
+ *  |RGB_TOG|      |      | Lclk | Rclk |                                  |VolUp |VolDn | Mute |      |RGB_MOD|
+ *  `-----------------------------------'                                  `-----------------------------------'
  *                                        ,-------------.  ,-------------.
  *                                        |      |      |  |      |      |
  *                                 ,------|------|------|  |------+------+------.
@@ -115,20 +115,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 // MEDIA AND MOUSE
 [MDIA] = LAYOUT_ergodox(
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2,
+       RGB_HUI, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       RGB_HUD, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS,
+       RGB_SAI, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,
+       RGB_SAD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       RGB_TOG, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2,
                                            KC_TRNS, KC_TRNS,
                                                     KC_TRNS,
                                   KC_TRNS, KC_TRNS, KC_TRNS,
     // right hand
        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY,
-       KC_TRNS,  KC_TRNS, KC_TRNS, KC_MPRV, KC_MNXT, KC_TRNS, KC_TRNS,
-                          KC_VOLU, KC_VOLD, KC_MUTE, KC_TRNS, KC_TRNS,
+                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_VAI,
+       KC_TRNS,  KC_TRNS, KC_TRNS, KC_MPRV, KC_MNXT, KC_TRNS, RGB_VAD,
+                          KC_VOLU, KC_VOLD, KC_MUTE, KC_TRNS, RGB_MOD,
        KC_TRNS, KC_TRNS,
        KC_TRNS,
        KC_TRNS, KC_TRNS, KC_WBAK
