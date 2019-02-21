5b00984fc3d6de33df84e4b02ba77fc84fcdf7dd
Cole Harrison <Douglas.Harrison@microsoft.com>
2019-02-21 15:47:44 -0800

    Added oneshot keys which lock shift and control until after next key stroke or on double press until next press

--- keyboards/ergodox_ez/keymaps/roanuk/config.h
+++ keyboards/ergodox_ez/keymaps/roanuk/config.h
@@ -2,5 +2,6 @@
 #define CONFIG_ROANUK_H
 
 #define RGB_MATRIX_KEYPRESSES
-
+#define ONESHOT_TAP_TOGGLE 2  /* Tapping this number of times holds the key until tapped this number of times again. */
+#define ONESHOT_TIMEOUT 30000  /* Time (in ms) before the one shot key is released */
 #endif
