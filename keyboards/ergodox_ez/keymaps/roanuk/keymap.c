55e5462be82e17276c219d3c2741e84ddc13096e
Cole Harrison <Douglas.Harrison@microsoft.com>
2019-02-21 14:44:09 -0800

    swapped symbols

--- keyboards/ergodox_ez/keymaps/roanuk/keymap.c
+++ keyboards/ergodox_ez/keymaps/roanuk/keymap.c
@@ -14,9 +14,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.      ,--------------------------------------------------.
  * |  =   + |  1 ! |  2 @ |  3 # |  4 % |  5 % |  L2  |      |  L2  |  6 ^ |  7 & |  8 * |  9 ( |  0 ) |  - _   |
  * |--------+------+------+------+------+-------------|      |------+------+------+------+------+------+--------|
- * |  #   * |   Q  |   W  |   F  |   P  |   G  |  L1  |      |  L1  |   J  |   L  |   U  |   Y  |   ;  |  \ |   |
+ * |  $   @ |   Q  |   W  |   F  |   P  |   G  |  L1  |      |  L1  |   J  |   L  |   U  |   Y  |   ;  |  \ |   |
  * |--------+------+------+------+------+------|      |      |      |------+------+------+------+------+--------|
- * |  $   @ |   A  |   R  |   S  |   T  |   D  |------|      |------|   H  |   N  |   E  |   I  |   O  |  ' "   |
+ * |  *   # |   A  |   R  |   S  |   T  |   D  |------|      |------|   H  |   N  |   E  |   I  |   O  |  ' "   |
  * |--------+------+------+------+------+------|      |      |      |------+------+------+------+------+--------|
  * | LShift |CT_T Z|   X  |   C  |   V  |   B  |  L3  |      |  L3  |   K  |   M  |  , <  | . > |  / ? | RShift |
  * `--------+------+------+------+------+-------------'      `-------------+------+------+------+------+--------'
@@ -35,10 +35,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
         KC_EQL,          KC_1,         KC_2,          KC_3,    KC_4, KC_5,    TG(CTRL),
-        KC_HASH,         KC_Q,         KC_W,          KC_F,    KC_P, KC_G,    TG(SHFT),
-        KC_DLR,          KC_A,         KC_R,          KC_S,    KC_T, KC_D,
-        TG(SHFT), CTL_T(KC_Z),  KC_X,          KC_C,    KC_V, KC_B,    TG(SYMB),
-        TG(CTRL), KC_QUOT,      KC_GRV, KC_LEFT, KC_RGHT,
+        KC_DLR,         KC_Q,         KC_W,          KC_F,    KC_P, KC_G,    TG(SHFT),
+        KC_ASTR,          KC_A,         KC_R,          KC_S,    KC_T, KC_D,
+        KC_LSFT, CTL_T(KC_Z),  KC_X,          KC_C,    KC_V, KC_B,    TG(SYMB),
+        KC_LCTL, KC_QUOT,      KC_GRV, KC_LEFT, KC_RGHT,
                                                                CTL_T(KC_APP), KC_HOME,
                                                                               KC_END,
                                                       KC_SPC,  KC_BSPC,       KC_DELT,
@@ -46,8 +46,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              TG(CTRL),KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS,
              TG(SHFT),    KC_J,   KC_L,   KC_U,    KC_Y,    KC_SCLN, KC_BSLS,
                           KC_H,   KC_N,   KC_E,    KC_I,    KC_O,    KC_QUOT,
-             TG(SYMB),    KC_K,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, TG(SHFT),
-                                  KC_UP,  KC_DOWN, KC_LBRC, KC_RBRC, TG(CTRL),
+             TG(SYMB),    KC_K,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                                  KC_UP,  KC_DOWN, KC_LBRC, KC_RBRC, KC_RCTL,
              KC_LWIN,     SFT_T(KC_ESC),
              KC_PGUP,
              KC_PGDN,     KC_TAB, ALT_T(KC_ENT)
@@ -55,8 +55,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [SHFT] = LAYOUT_ergodox(  // layer 2 : shift + base layer
         // left hand
         KC_PLUS, KC_EXLM,    KC_AT,      KC_HASH,    KC_DLR,     KC_PERC,    KC_TRNS,
-        KC_ASTR, LSFT(KC_Q), LSFT(KC_W), LSFT(KC_F), LSFT(KC_P), LSFT(KC_G), KC_TRNS,
-        KC_AT,   LSFT(KC_A), LSFT(KC_R), LSFT(KC_S), LSFT(KC_T), LSFT(KC_D),
+        KC_AT, LSFT(KC_Q), LSFT(KC_W), LSFT(KC_F), LSFT(KC_P), LSFT(KC_G), KC_TRNS,
+        KC_HASH,   LSFT(KC_A), LSFT(KC_R), LSFT(KC_S), LSFT(KC_T), LSFT(KC_D),
         KC_TRNS, LSFT(KC_Z), LSFT(KC_X), LSFT(KC_C), LSFT(KC_V), LSFT(KC_B), KC_TRNS,
         KC_TRNS, KC_TRNS,    KC_TILD,    KC_TRNS,    KC_TRNS,
                                                                  KC_TRNS,    KC_TRNS,
