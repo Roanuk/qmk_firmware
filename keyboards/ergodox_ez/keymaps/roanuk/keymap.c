74e3c9594bb0ed9e792fed48a954aa649cad3ad2
Cole Harrison <douglas.cole.harrison@gmail.com>
2019-05-09 13:42:47 -0700

    Added swap hands keys, added symbol based num pad, removed ctrl + layer, modified led layers display to be more informative

--- keyboards/ergodox_ez/keymaps/roanuk/keymap.c
+++ keyboards/ergodox_ez/keymaps/roanuk/keymap.c
@@ -4,21 +4,20 @@
 
 #define BASE 0 // default layer
 #define SHFT 1 // Shift +
-#define CTRL 2 // Control +
-#define SYMB 3 // symbols
-#define MDIA 4 // media keys
+#define SYMB 2 // symbols
+#define MDIA 3 // media
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.      ,--------------------------------------------------.
- * |  =   + |  1 ! |  2 @ |  3 # |  4 % |  5 % |  L2  |      |  L2  |  6 ^ |  7 & |  8 * |  9 ( |  0 ) |  - _   |
+ * |  =   + |  1 ! |  2 @ |  3 # |  4 $ |  5 % | Swap |      | Swap |  6 ^ |  7 & |  8 * |  9 ( |  0 ) |  - _   |
  * |--------+------+------+------+------+-------------|      |------+------+------+------+------+------+--------|
- * |  $   @ |   Q  |   W  |   F  |   P  |   G  |  L1  |      |  L1  |   J  |   L  |   U  |   Y  |   ;  |  \ |   |
+ * |  $   @ |   Q  |   W  |   F  |   P  |   G  |  L1  |      |  L1  |   J  |   L  |   U  |   Y  |  ; : |  \ |   |
  * |--------+------+------+------+------+------|      |      |      |------+------+------+------+------+--------|
  * |  *   # |   A  |   R  |   S  |   T  |   D  |------|      |------|   H  |   N  |   E  |   I  |   O  |  ' "   |
  * |--------+------+------+------+------+------|      |      |      |------+------+------+------+------+--------|
- * | LShift |CT_T Z|   X  |   C  |   V  |   B  |  L3  |      |  L3  |   K  |   M  |  , <  | . > |  / ? | RShift |
+ * | LShift |CT_T Z|   X  |   C  |   V  |   B  |  L2  |      |  L2  |   K  |   M  |  , <  | . > |  / ? | RShift |
  * `--------+------+------+------+------+-------------'      `-------------+------+------+------+------+--------'
  *   |LCtrl |  ' " |  ` ~ | Left | Right|                                  |  Up  | Down |  [ { |  ] } | RCtrl|
  *   `----------------------------------'                                  `----------------------------------'
@@ -34,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_EQL,          KC_1,         KC_2,          KC_3,    KC_4, KC_5,    TG(CTRL),
+        KC_EQL,          KC_1,         KC_2,          KC_3,    KC_4, KC_5,    SH_TT,
         KC_DLR,         KC_Q,         KC_W,          KC_F,    KC_P, KC_G,    TG(SHFT),
         KC_ASTR,          KC_A,         KC_R,          KC_S,    KC_T, KC_D,
         OSM(MOD_LSFT), CTL_T(KC_Z),  KC_X,          KC_C,    KC_V, KC_B,    TG(SYMB),
@@ -43,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                               KC_END,
                                                       KC_SPC,  KC_BSPC,       KC_DELT,
         // right hand
-             TG(CTRL),KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS,
+             SH_TT,KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS,
              TG(SHFT),    KC_J,   KC_L,   KC_U,    KC_Y,    KC_SCLN, KC_BSLS,
                           KC_H,   KC_N,   KC_E,    KC_I,    KC_O,    KC_QUOT,
              TG(SYMB),    KC_K,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, OSM(MOD_RSFT),
@@ -72,41 +71,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_TRNS,
              KC_TRNS,  KC_TRNS,    KC_TRNS
     ),
-[CTRL] = LAYOUT_ergodox(  // layer 1 : control + base layer
-        // left hand
-        KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
-        KC_TRNS, LCTL(KC_Q), LCTL(KC_W), LCTL(KC_F), LCTL(KC_P), LCTL(KC_G), KC_TRNS,
-        KC_TRNS, LCTL(KC_A), LCTL(KC_R), LCTL(KC_S), LCTL(KC_T), LCTL(KC_D),
-        KC_TRNS, LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), LCTL(KC_B), KC_TRNS,
-        KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
-                                                                 KC_TRNS,    KC_TRNS,
-                                                                             KC_TRNS,
-                                                     KC_TRNS,    KC_TRNS,    KC_TRNS,
-        // right hand
-             KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
-             KC_TRNS,  LCTL(KC_J), LCTL(KC_L), LCTL(KC_U), LCTL(KC_Y), KC_TRNS,    KC_TRNS,
-                       LCTL(KC_H), LCTL(KC_N), LCTL(KC_E), LCTL(KC_I), LCTL(KC_O), KC_TRNS,
-             KC_TRNS,  LCTL(KC_K), LCTL(KC_M), KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
-                                   KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
-             KC_TRNS,   KC_TRNS,
-             KC_TRNS,
-             KC_TRNS,   KC_TRNS, KC_TRNS
-    ),
-/* Keymap 3: Symbol Layer
+/* Keymap 2: Symbol Layer
 *
 * ,--------------------------------------------------.      ,--------------------------------------------------.
-* |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |      |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
+* |    +   |  F1  |  F2  |  F3  |  F4  |  F5  |      |      |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
 * |--------+------+------+------+------+-------------|      |------+------+------+------+------+------+--------|
-* |        |   !  |   @  |   {  |   }  |   |  |      |      |      |   Up |   7  |   8  |   9  |   *  |   F12  |
+* |    &   |   *  |   |  |   {  |   }  | MsLt |      |      |      | MsUp |   7  |   8  |   9  |   *  |   F12  |
 * |--------+------+------+------+------+------|      |      |      |------+------+------+------+------+--------|
-* |        |   &  |   *  |   (  |   )  |   `  |------|      |------| Down |   4  |   5  |   6  |   +  |        |
+* |    $   |   %  |   ^  |   (  |   )  | MsRt |------|      |------| MsDn |   4  |   5  |   6  |   +  |   -    |
 * |--------+------+------+------+------+------|      |      |      |------+------+------+------+------+--------|
-* |        |   %  |   ^  |   [  |   ]  |   ~  |      |      |      |   .  |   1  |   2  |   3  |   \  |        |
+* |    !   |   @  |   #  |   [  |   ]  | MsLc |      |      |      |   .  |   1  |   2  |   3  |   /  |   ?    |
 * `--------+------+------+------+------+-------------'      `-------------+------+------+------+------+--------'
-*  |       |      |      |      |      |                                  |      |      |   0  |   =  |       |
+*  |   =   |   <  |   >  |      |      |                                  |      |      |   0  |   =  |   \   |
 *  `-----------------------------------'                                  `-----------------------------------'
 *                                        ,-------------.  ,-------------.
-*                                        |Ctr+X |  L4  |  |  L4  | Ctr+S|
+*                                        |Ctr+X |  L3  |  |  L3  | Ctr+S|
 *                                 ,------|------|------|  |------+------+------.
 *                                 |      |      |Ctr+C |  |Ctr+Y |      |      |
 *                                 |      |      |------|  |------|      |      |
@@ -116,25 +95,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // SYMBOLS
 [SYMB] = LAYOUT_ergodox(
        // left hand
-       KC_TRNS,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_TRNS,
-       KC_TRNS,KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,KC_TRNS,
-       KC_TRNS,KC_AMPR,KC_ASTR, KC_LPRN,KC_RPRN,KC_GRV,
-       KC_TRNS,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,KC_TRNS,
-       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
+       KC_PPLS,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_TRNS,
+       KC_AMPR,KC_ASTR,KC_PIPE,  KC_LCBR,KC_RCBR,KC_MS_L,KC_TRNS,
+       KC_DLR,KC_PERC,KC_CIRC, KC_LPRN,KC_RPRN,KC_MS_R,
+       KC_EXLM,KC_AT,KC_HASH,KC_LBRC,KC_RBRC,KC_BTN1,KC_TRNS,
+       KC_PEQL,KC_LABK,KC_RABK,KC_TRNS,KC_TRNS,
                                        LCTL(KC_X),TG(MDIA),
                                                LCTL(KC_V),
                                KC_TRNS,KC_TRNS,LCTL(KC_C),
        // right hand
        KC_TRNS, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
-       KC_TRNS, KC_UP,   KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
-                KC_DOWN, KC_4,   KC_5,    KC_6,    KC_PLUS, KC_TRNS,
-       KC_TRNS, KC_DOT,  KC_1,   KC_2,    KC_3,    KC_BSLS, KC_TRNS,
-                         KC_TRNS,KC_TRNS, KC_0,    KC_EQL,  KC_TRNS,
+       KC_TRNS, KC_MS_U,   KC_7,   KC_8,    KC_9,    KC_PAST, KC_F12,
+                KC_MS_D, KC_4,   KC_5,    KC_6,    KC_PPLS, KC_PMNS,
+       KC_TRNS, KC_DOT,  KC_1,   KC_2,    KC_3,    KC_PSLS, KC_QUES,
+                         KC_TRNS,KC_TRNS, KC_0,    KC_PEQL,  KC_BSLS,
        TG(MDIA), LCTL(KC_S),
        LCTL(KC_Y),
        LCTL(KC_Z), KC_TRNS, KC_TRNS
 ),
-/*  Keymap 2: Media and mouse keys
+ /*  Keymap 2: Media and mouse keys
  *
  * ,--------------------------------------------------.      ,--------------------------------------------------.
  * | RGB_HUI|      |      |      |      |      |      |      |      |      |      |      |      |      |        |
@@ -173,7 +152,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS, KC_TRNS,
        KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS
-),
+)
 };
 
 const uint16_t PROGMEM fn_actions[] = {
@@ -202,32 +181,23 @@ void matrix_init_user(void) {
 
 // Runs constantly in the background, in a loop.
 void matrix_scan_user(void) {
-
     uint8_t layer = biton32(layer_state);
 
     ergodox_board_led_off();
     ergodox_right_led_1_off();
     ergodox_right_led_2_off();
     ergodox_right_led_3_off();
-    switch (layer) {
-      // TODO: Make this relevant to the ErgoDox EZ.
-        case 4:
-            ergodox_right_led_2_on();
-            break;
-        case 3:
-            ergodox_right_led_1_on();
-            break;
-	    case 2:
-            ergodox_right_led_2_on();
-			ergodox_right_led_3_on();
-            break;
-		case 1:
-            ergodox_right_led_1_on();
-			ergodox_right_led_3_on();
-            break;
-        default:
-            // none
-            break;
-    }
-
+	
+	if(layer & SYMB)
+	{
+		ergodox_right_led_1_on();		
+	}
+	if(layer & SHFT)
+	{
+		ergodox_right_led_2_on();
+	}	
+	if(layer & MDIA)
+	{
+		ergodox_right_led_3_on();
+	}
 };
